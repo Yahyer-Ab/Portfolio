24131aadb77edd72b6b4a6c06cfcc1b598e7d75b
Yahyer-Ab <yahyerabdi@gmail.com>
2024-09-27 18:29:41 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Typing animation
+var typed = new Typed(".typing", {
+    strings: [
+        "",
+        "BBIT Graduate",
+        "Software Developer",
+        "ICT Support Specialist",
+        "Web Developer",
+        "Graphic Designer",
+        "Software Installation & Troubleshooting Expert",
+        "Web Application Designer",
+        "Digital Card & Logo Designer"
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+(emailjs.init('uvpGB3yumECW6UHCC'));
